e37ea933c97528bd888ac3e5e1778e7cf961fa2e
Yehonal <yehonal.azeroth@gmail.com>
2016-08-31 03:23:08 +0200

    [AZTH] cleaning

--- modules/azerothshard/bin/bash_shared/includes.sh
+++
@@ -1,3 +0,0 @@
-CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source "$CURRENT_PATH/../../../azth/bin/compiler/includes/includes.sh"
--- modules/azerothshard/bin/compiler/includes/include.sh
+++
@@ -1,5 +0,0 @@
-source "../../bash_shared/includes.sh"
-
-AZTH_PATH_CUSTOM_COMPILER="$AZTH_PATH_CUSTOM/bin/compiler"
-
-source "$AZTH_PATH_CUSTOM_COMPILER/includes/custom.sh"
