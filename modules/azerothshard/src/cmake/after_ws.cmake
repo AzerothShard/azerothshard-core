d66ba422559c532704f81a838cc66ba991a52d95
Yehonal <yehonal.azeroth@gmail.com>
2016-09-14 01:01:15 +0200

    [AZTH] windows post build command for azth_mod.conf

--- modules/azerothshard/src/cmake/after_ws.cmake
+++ modules/azerothshard/src/cmake/after_ws.cmake
@@ -1,5 +1,13 @@
-if( UNIX )
-  install(FILES "${CMAKE_AZTH_DIR}/conf/azth_mod.conf.dist" DESTINATION ${CONF_DIR})
-elseif( WIN32 )
-  install(FILES "${CMAKE_AZTH_DIR}/conf/azth_mod.conf.dist" DESTINATION "${CMAKE_INSTALL_PREFIX}")
+if ( MSVC )
+  add_custom_command(TARGET worldserver
+    POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_AZTH_DIR}/conf/azth_mod.conf.dist ${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/
+  )
+elseif ( MINGW )
+  add_custom_command(TARGET worldserver
+    POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_AZTH_DIR}/conf/azth_mod.conf.dist ${CMAKE_BINARY_DIR}/bin/
+  )
 endif()
+
+install(FILES "${CMAKE_AZTH_DIR}/conf/azth_mod.conf.dist" DESTINATION ${CONF_DIR})
