d5b1ab4ec84cb3a9035070076c516d736fe91b5b
Yehonal <yehonal.azeroth@gmail.com>
2016-11-17 19:11:30 +0100

    [AZTH][DEV-OP] db: Guild houses commands

--- modules/azerothshard/data/sql/db-world/azth_commands.sql
+++ modules/azerothshard/data/sql/db-world/azth_commands.sql
@@ -7,3 +7,10 @@ INSERT INTO trinity_string(entry, content_default) VALUES(90001, 'The target lev
 -- xp rate
 DELETE FROM command WHERE name = 'azth xp';
 INSERT INTO command VALUES('azth xp', 0, 'Set a custom xp rate');
+
+-- guild houses
+
+INSERT INTO `command` (`name`, `security`, `help`) VALUES ('npc guildadd', '2', 'Sintassi: \r\n\r\nnpc guildadd '); 
+UPDATE `command` SET `help` = 'Syntax: \r\n\r\nnpc guildadd [creature] [guild_house_id] [guild_house_add_id]' WHERE `name` = 'npc guildadd'; 
+INSERT INTO `command` (`name`, `security`) VALUES ('gobject guildadd', '2'); 
+UPDATE `command` SET `help` = 'Syntax: \r\n\r\ngobject guildadd [object] [guild_house_id] [guild_house_add_id]' WHERE `name` = 'gobject guildadd'; 
