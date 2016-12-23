ce8eb3feb274374aa1fc7eac04f21325822da0c7
Matteo Emili <matteo.emili14@gmail.com>
2016-12-23 20:44:25 +0100

    [AZTH] Improvements for jukebox

--- modules/Jukebox/src/Jukebox.cpp
+++ modules/Jukebox/src/Jukebox.cpp
@@ -4,6 +4,7 @@
 #include "ItemPrototype.h"
 #include "Common.h"
 #include "music.h"
+#include "ScriptedCreature.h"
 
 std::vector<music> musicList;
 
@@ -33,29 +34,42 @@ public:
 class Jukebox : public CreatureScript
 {
 public:
-  Jukebox() : CreatureScript("Jukebox") {}
-  
-  bool OnGossipHello(Player* player, Creature* creature)
-  {
-	  for (uint32 i = 0; i < musicList.size(); i++)
-	  {
-		  player->ADD_GOSSIP_ITEM(0, musicList[i].GetAuthor() + " - " + musicList[i].GetTitle(), GOSSIP_SENDER_MAIN, i);
-	  }
-	  player->SEND_GOSSIP_MENU(1, creature->GetGUID());
-	  return true;
-  }
+	Jukebox() : CreatureScript("Jukebox") {}
 
+	bool OnGossipHello(Player* player, Creature* creature)
+	{
+		for (uint32 i = 0; i < musicList.size(); i++)
+		{
+			player->ADD_GOSSIP_ITEM(0, musicList[i].GetAuthor() + " - " + musicList[i].GetTitle(), GOSSIP_SENDER_MAIN, i);
+		}
+		player->SEND_GOSSIP_MENU(1, creature->GetGUID());
+		return true;
+	}
 
-  bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action)
-  {
-	  player->PlayerTalkClass->ClearMenus();
-	  if (action <= musicList.size())
-	  {
-		  creature->PlayDistanceSound(musicList[action].GetMusicId());
-	  }
-	  player->PlayerTalkClass->SendCloseGossip();
-	  return true;
-  }
+	bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action)
+	{
+		player->PlayerTalkClass->ClearMenus();
+		if (action <= musicList.size())
+		{
+			creature->PlayDistanceSound(musicList[action].GetMusicId());
+			std::string msg = "Now playing: " + musicList[action].GetAuthor() + " - " + musicList[action].GetTitle();
+			creature->MonsterSay(msg.c_str(), 0, creature);
+		}
+		player->PlayerTalkClass->SendCloseGossip();
+		return true;
+	}
+};
+
+class JukeboxSummon : public ItemScript
+{
+public:
+	JukeboxSummon() : ItemScript("JukeboxSummon") {}
+
+	bool OnUse(Player* player, Item* item, SpellCastTargets const& targets)
+	{
+		player->SummonCreature(300205, player->GetPositionX(), player->GetPositionY(), player->GetPositionZ(), player->GetOrientation(), TEMPSUMMON_TIMED_DESPAWN, 10 * 60 * IN_MILLISECONDS);
+		return true;
+	}
 };
 
 
@@ -63,4 +77,5 @@ void AddSC_Jukebox()
 {
 	new loadMusic();
 	new Jukebox();
+	new JukeboxSummon();
 }
