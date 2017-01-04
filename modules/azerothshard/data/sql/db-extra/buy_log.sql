d9753882a2dfd66f15c81b07c1dfe1a65f914b97
Gargarensis <gabririchi@yahoo.it>
2017-01-04 02:10:09 +0100

    log for buys + smartstone adaptation

---
+++ modules/azerothshard/data/sql/db-extra/buy_log.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `buy_log`;
+CREATE TABLE `buy_log` (
+  `playerGuid` BIGINT(10) NOT NULL,
+  `account` BIGINT(10) NOT NULL,
+  `item` INT(10) NOT NULL,
+  `vendor` BIGINT(10) NOT NULL,
+  `price` BIGINT(10) NOT NULL COMMENT 'if < 0 it is an extendedCost'
+);
