0d5b8e0d88b3622a6f6e6ebcfc0179c696913f89
Yehonal <yehonal.azeroth@gmail.com>
2017-01-28 13:26:07 +0100

    [AZTH] Changed some items scaling values for Timewalking

--- modules/azerothshard/src/game/AzthUtils.cpp
+++ modules/azerothshard/src/game/AzthUtils.cpp
@@ -48,73 +48,101 @@ uint32 AzthUtils::calculateItemScalingValue(ItemTemplate const * pProto, Player 
     if (pProto->RequiredLevel <= pl->getLevel()) // remove / apply
         return 0;
 
-    uint32 mul = 1; // should be 1 ?
+    //uint32 mul = 4; // should be 1 ?
 
     // SHOULDERS
     if (pProto->InventoryType == INVTYPE_SHOULDERS) { 
         if (pProto->Class == ITEM_CLASS_ARMOR) {
             switch (pProto->SubClass) {
             case ITEM_SUBCLASS_ARMOR_CLOTH :
-                return mul + 32;
+                return 1 + 32;
             case ITEM_SUBCLASS_ARMOR_LEATHER :
-                return mul + 64;
+                return 1 + 64;
             case ITEM_SUBCLASS_ARMOR_MAIL :
-                return mul + 128;
+                return 1 + 128;
             case ITEM_SUBCLASS_ARMOR_PLATE :
-                return mul + 256;
+                return 1 + 256;
             }
         }
 
         return 0;
     }
+    
+        
+    if (pProto->InventoryType == INVTYPE_CHEST || pProto->InventoryType == INVTYPE_ROBE) {
+        switch (pProto->SubClass) {
+        case ITEM_SUBCLASS_ARMOR_CLOTH:
+            return 8 + 1048576;
+        case ITEM_SUBCLASS_ARMOR_LEATHER:
+            return 8 + 2097152;
+        case ITEM_SUBCLASS_ARMOR_MAIL :
+            return 8 + 4194304;
+        case ITEM_SUBCLASS_ARMOR_PLATE:
+            return 8 + 8388608;
+        }
+        
+        return 0;
+    }
 
     // WEAPONS 
     if (pProto->InventoryType == INVTYPE_2HWEAPON) {
-        return mul + 1024;
+        if (pProto->Class == ITEM_CLASS_WEAPON && pProto->SubClass == ITEM_SUBCLASS_WEAPON_STAFF ) {
+            return 8 + 4096 + 32768;
+        } else {
+            return 8 + 1024;
+        }
     }
 
     if (pProto->InventoryType == INVTYPE_WEAPON || pProto->InventoryType == INVTYPE_WEAPONMAINHAND
         || pProto->InventoryType == INVTYPE_WEAPONOFFHAND) {
-        return mul + 512; // should be 4 ?
+        return 4 + 512; // should be 4 ?
     }
 
     // RANGED
     if (pProto->InventoryType == INVTYPE_RANGED || pProto->InventoryType == INVTYPE_AMMO || pProto->InventoryType == INVTYPE_THROWN)
-        return mul + 8192; // should be 16 ?
+        return 16 + 8192; // should be 16 ?
         
     // WANDS
     if (pProto->InventoryType == INVTYPE_RANGEDRIGHT)
-        return mul + 16384; // should be 16 ?
+        return 16 + 16384; // should be 16 ?
 
 
+    if (pProto->InventoryType == INVTYPE_TRINKET) {
+        return 2; // should be 2 ?
+    }
+
+   
+    if (pProto->InventoryType == INVTYPE_FINGER)
+        return 262144;
+    
+    
+    if (pProto->InventoryType == INVTYPE_NECK)
+        return 262144;
+    
+    //
+    // special unknown cases
+    //
+    
     // CLOAK
     if (pProto->InventoryType == INVTYPE_CLOAK) {
-        return mul + 524288;
+        return 4 + 524288;
     }
     
     // ARMOR
     if (pProto->InventoryType == INVTYPE_SHIELD) {
-        return mul + 8388608;
+        return 4 + 8388608;
     }
-
-    if (pProto->InventoryType == INVTYPE_TRINKET) {
-        return mul; // should be 2 ?
-    }
-
-    // special cases
-    if (pProto->InventoryType == INVTYPE_FINGER || pProto->InventoryType == INVTYPE_NECK)
-        return mul;
-
+    
     if (pProto->Class == ITEM_CLASS_ARMOR) {
         switch (pProto->SubClass) {
         case ITEM_SUBCLASS_ARMOR_CLOTH:
-            return mul + 1048576;
+            return 4 + 1048576;
         case ITEM_SUBCLASS_ARMOR_LEATHER:
-            return mul + 2097152;
+            return 4 + 2097152;
         case ITEM_SUBCLASS_ARMOR_MAIL :
-            return mul + 4194304;
+            return 4 + 4194304;
         case ITEM_SUBCLASS_ARMOR_PLATE:
-            return mul + 8388608;
+            return 4 + 8388608;
         }
     }
 
