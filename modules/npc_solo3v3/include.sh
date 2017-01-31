7e1a1a2b40fb4c83445481f339a9ec9a7cb5954e
Matteo Emili <matteo.emili14@gmail.com>
2017-01-31 04:17:20 +0100

    Revert "[AZTH] SoloQ 3v3 v2"
    This reverts commit 0d986a45257eca9dd29fa06e520db76223835a4b.
    
    # Conflicts:
    #	.gitignore

--- modules/npc_solo3v3/include.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-JUKEBOX_MODULE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
-
-DB_WORLD_CUSTOM_PATHS+=(
-    $JUKEBOX_MODULE_PATH"/data/sql/*"
-)
--- modules/npc_solo3v3/src/loader.h
+++
@@ -1,5 +0,0 @@
-void AddSC_npc_solo3v3();
-
-void Addnpc_solo3v3Scripts() {
-	AddSC_npc_solo3v3();
-}
