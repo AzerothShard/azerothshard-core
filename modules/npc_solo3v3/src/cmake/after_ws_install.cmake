4572c80526770f9fe28df7632f1b1669a4a472b9
Matteo Emili <matteo.emili14@gmail.com>
2017-02-20 17:36:08 +0100

    [AZTH] 3v3 SoloQ config

---
+++ modules/npc_solo3v3/src/cmake/after_ws_install.cmake
@@ -0,0 +1,13 @@
+if ( MSVC )
+  add_custom_command(TARGET worldserver
+    POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_3V3SOLOQ_DIR}/conf/3v3_soloq.conf.dist ${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/
+  )
+elseif ( MINGW )
+  add_custom_command(TARGET worldserver
+    POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_3V3SOLOQ_DIR}/conf/3v3_soloq.conf.dist ${CMAKE_BINARY_DIR}/bin/
+  )
+endif()
+
+install(FILES "${CMAKE_DUELRESET_DIR}/conf/3v3_soloq.conf.dist" DESTINATION ${CONF_DIR})
