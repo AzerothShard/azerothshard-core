61daa5357ed60287afdbbe8c0d1005fd6a818976
Yehonal <yehonal.azeroth@gmail.com>
2017-07-31 15:02:40 +0000

    [AZTH] moved sql to PvPMode module
    AzerothTournament is a legacy module
    
    # Conflicts:
    #	modules/PvPMode/include.sh
    
    # Conflicts:
    #	modules/PvPMode/data/sql/world/trainers.sql
    #	modules/PvPMode/include.sh

--- modules/PvPMode/include.sh
+++ modules/PvPMode/include.sh
@@ -5,3 +5,7 @@ PVPMODE_MODULE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 DB_EXTRA_CUSTOM_PATHS+=(
     $PVPMODE_MODULE_PATH"/data/sql/extra/"
 )
+
+DB_WORLD_CUSTOM_PATHS+=(
+    $PVPMODE_MODULE_PATH"/data/sql/world"
+)
